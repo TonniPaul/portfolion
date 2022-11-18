dab21aad3328c7b0ea48032ef9a16ad7aff19e69
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-11-18 23:34:10 +0100

    I added more skill and skill description on hover

--- src/Object Files/skillData.js
+++ src/Object Files/skillData.js
@@ -5,36 +5,49 @@ const skillData = [
     id: uuid(),
     skillImage: "./assets/react.png",
     logo: "react logo",
+    logo_text: "react",
   },
   {
     id: uuid(),
     skillImage: "./assets/html.png",
     logo: "react logo",
+    logo_text: "html5",
   },
   {
     id: uuid(),
     skillImage: "./assets/css.png",
     logo: "react logo",
+    logo_text: "css",
   },
   {
     id: uuid(),
     skillImage: "./assets/js.png",
     logo: "react logo",
+    logo_text: "javascript",
+  },
+  {
+    id: uuid(),
+    skillImage: "./assets/wordpress.png",
+    logo: "react logo",
+    logo_text: "wordpress",
   },
   {
     id: uuid(),
     skillImage: "./assets/bootstrap.png",
     logo: "react logo",
+    logo_text: "bootstrap",
   },
   {
     id: uuid(),
     skillImage: "./assets/git.png",
     logo: "react logo",
+    logo_text: "git/ version control",
   },
   {
     id: uuid(),
     skillImage: "./assets/github.png",
     logo: "react logo",
+    logo_text: "github",
   },
 ];
 
--- src/cards/circleCards/CircleCard.jsx
+++ src/cards/circleCards/CircleCard.jsx
@@ -1,9 +1,12 @@
 import "./circle-card.css";
 
-const CircleCard = ({ skillImage }) => {
+const CircleCard = ({ skillImage, logo_text, logo }) => {
   return (
-    <div className="circle-card-cont">
-      <img src={skillImage} alt="" className="circle-img" />
+    <div className="circle">
+      <div className=" flex column circle-card-cont">
+        <img src={skillImage} alt={logo} className="circle-img" />
+      </div>
+      <p className="logo_name">{logo_text}</p>
     </div>
   );
 };
