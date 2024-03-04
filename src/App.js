082b15b7cadff1f2f6d741d0ae5f7f5c49887cd3
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-03-04 15:43:56 +0100

    refactor: reorder app.js imports and add eslintCache to ignore file

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
-import "./App.css";
-import NavBar from "./components/navbar/NavBar";
-import { useEffect, useState } from "react";
-import ScrollToTop from "./components/ScrollToTop";
-import { Route, Routes } from "react-router-dom";
-import ErrorPage from "./components/Error/Error";
-import Main from "./pages/Main";
-import { Analytics } from "@vercel/analytics/react";
+import { useEffect, useState } from 'react';
+import { Analytics } from '@vercel/analytics/react';
+import { Route, Routes } from 'react-router-dom';
+
+import NavBar from './components/navbar/NavBar';
+import ScrollToTop from './components/ScrollToTop';
+import ErrorPage from './components/Error/Error';
+import Main from './pages/Main';
+
+import './App.css';
 
 function App() {
   // Toggle Theme Function
   const [theme, setTheme] = useState(
-    localStorage.getItem("themeColor")
-      ? localStorage.getItem("themeColor").toString()
-      : "light"
+    localStorage.getItem('themeColor')
+      ? localStorage.getItem('themeColor').toString()
+      : 'light'
   );
 
   const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+    setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
   };
 
   useEffect(() => {
-    localStorage.setItem("themeColor", theme);
+    localStorage.setItem('themeColor', theme);
   }, [theme]);
 
   return (
-    <div className='App' id={theme}>
+    <div className="App" id={theme}>
       <NavBar onClick={toggleTheme}>
         <img
           src={theme === 'light' ? '/assets/sun.png' : '/assets/moon.png'}
-          alt=''
-          className='switchIcon'
+          alt=""
+          className="switchIcon"
           width={20}
           height={20}
         />
       </NavBar>
 
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='*' element={<ErrorPage />} />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
 
       {/* SCROLL TO TOP ICON  */}
       <ScrollToTop />
-      <footer className='flex column footer'>
+      <footer className="flex column footer">
         <small> &copy; {new Date().getFullYear()}, all rights reserved</small>
         <small>
           Designed & Built by{' '}
-          <span className='alt-text TNP'>TonniPaul&#xae;</span>
+          <span className="alt-text TNP">TonniPaul&#xae;</span>
         </small>
       </footer>
       <Analytics />
