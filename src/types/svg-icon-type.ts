6c4dd12730b117fe7f7aa9941a1e8ad8a8822c71
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-03-17 15:13:56 +0100

    feat: create svg icon sprite, component and types

---
+++ src/types/svg-icon-type.ts
@@ -0,0 +1,13 @@
+export type ISvgIconNameTypes =
+  | 'next'
+  | 'previous'
+  | 'moon'
+  | 'sun'
+  | 'whatsapp'
+  | 'call'
+  | 'instagram'
+  | 'mail'
+  | 'x-twitter'
+  | 'linkedin'
+  | 'github'
+  | 'download';
