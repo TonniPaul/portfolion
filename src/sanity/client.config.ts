8fd41ea7c70ea58433f48265773c4c0722e45eef
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-03-17 16:27:37 +0100

    refactor(snity-config):  Update Sanity client config to use constants from data/constants.

--- src/sanity/client.config.ts
+++ src/sanity/client.config.ts
@@ -1,8 +1,7 @@
 import imageUrlBuilder from '@sanity/image-url';
 import { createClient } from '@sanity/client';
 
-const SANITY_DATASET = process.env.REACT_APP_SANITY_DATASET as string;
-const SANITY_PROJECT_ID = process.env.REACT_APP_SANITY_PROJECT_ID as string;
+import { SANITY_DATASET, SANITY_PROJECT_ID } from '../data/constants';
 
 const clientConfig = {
   projectId: SANITY_PROJECT_ID,
